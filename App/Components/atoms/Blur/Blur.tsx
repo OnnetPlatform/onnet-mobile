4f530b99f3f017034d126e9b7dd83915d688e123
Muhammad Saleh <m7md.sal762@gmail.com>
2024-01-19 09:55:02 +0200

    update blur view

--- App/Components/atoms/Blur/Blur.tsx
+++ App/Components/atoms/Blur/Blur.tsx
@@ -1,12 +1,13 @@
 import { BlurView } from '@react-native-community/blur';
 import React from 'react';
 import { useColorScheme } from 'react-native';
+
 import { BlurProps } from './types';
 
 export const Blur: React.FC<BlurProps> = ({ children, ...props }) => {
   const isDark = useColorScheme() === 'dark';
   return (
-    <BlurView {...props} blurType={isDark ? 'dark' : 'light'}>
+    <BlurView {...props} blurType={isDark ? 'dark' : 'light'} blurAmount={1}>
       {children}
     </BlurView>
   );
--- App/Components/atoms/Blur/types.ts
+++ App/Components/atoms/Blur/types.ts
@@ -1,4 +1,6 @@
 import { BlurViewProps } from '@react-native-community/blur';
 import { ReactElement } from 'react';
 
-export type BlurProps = BlurViewProps & { children: ReactElement | ReactElement[] };
+export type BlurProps = BlurViewProps & {
+  children?: ReactElement | ReactElement[];
+};
